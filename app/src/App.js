f66d3e273dfcd42a24d776c96193abc3c69abf31
Jeongjoon Lee <joondev32@gmail.com>
2020-06-25 18:47:48 -0700

    feat: Add create functionality

--- app/src/App.js
+++ app/src/App.js
@@ -1,79 +1,69 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class Content extends Component {
+class Student extends Component {
     renderField = (data) => {
         if (data) {
             return (
                 <div>
-                    <li>Name: {data.name}</li>
+                    <h4>{data.name}</h4>
                     <li>Age: {data.age}</li>
                     <li>GPA: {data.gpa}</li>
+
+                    <input type="button" value="Edit"></input>
+                    <input type="button" value="Delete"></input>
                 </div>
             );
         }
     };
 
     render() {
-        console.log("Content Render");
+        console.log("Student Render");
         return (
-            <div className="Content">
+            <div className="Student">
                 {this.renderField(this.props.data)}
             </div>
         );
     }
 }
 
-class BBS extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            selected_id: 0,
-        }
-    }
-
-    renderDataList = () => {
-        const data_list = this.props.data_list;
-
-        if (data_list.length === 0) {
-            return (<li>No Data</li>);
-        }
-
-        return data_list.map(data => (
-            <li key={data.id}><a href={"./" + data.id} onClick={function(e) {
-                e.preventDefault();
-                this.setState({
-                    selected_id: data.id,
-                });
-            }.bind(this)}>{data.name}</a></li>
-        ));
-    };
-
-    getData = (selected_id) => {
-        return this.props.data_list.find(data => data.id === selected_id);
-    }
-
+class StudentCreateForm extends Component {
     render() {
-        console.log("BBS Render");
+        console.log(this.props.current_category.toLocaleLowerCase())
+        console.log("StudentCreateForm render");
         return (
-            <div className="BBS">
-                <h2>{this.props.category}</h2>
+            <div className="CreateStudent">
+                <h1>Add Student</h1>
+                <form
+                    action=""
+                    method="post"
+                    onSubmit={function(e) {
+                        e.preventDefault();
+                        this.props.onSubmit(e.target.name.value, e.target.age.value, e.target.gpa.value);
+                     }.bind(this)}>
 
-                <nav>
-                    <ul>
-                        {this.renderDataList()}
-                    </ul>
-                </nav>
+                    <p><input type="text" name="name" placeholder="Name"></input></p>
+                    <p><input type="number" name="age" placeholder="Age"></input></p>
+                    <p><input type="number" step="0.1" name="gpa" placeholder="GPA"></input></p>
 
-                <Content data={this.getData(this.state.selected_id)}></Content>
+                    <label>Choose a category: </label>
+                    <select name="categories" defaultValue={this.props.current_category.toLocaleLowerCase()}>
+                        <option value="unknown">Unknown</option>
+                        <option value="person">Person</option>
+                        <option value="animal">Animal</option>
+                    </select>
+
+                    <p>
+                        <input  type="submit"></input>
+                    </p>
+                </form>
             </div>
         );
     }
 }
 
-class App extends Component {
-    ALL = 'All Student';
+class BBS extends Component {
+    ALL = 'Home';
     PERSON = 'Person';
     ANIMAL = 'Animal';
 
@@ -83,94 +73,186 @@ class App extends Component {
             "name": "Bob",
             "age": 3,
             "gpa": 3.0,
-            "category": this.PERSON,
+            "category": "person",
         },
         {
             "id": 2,
             "name": "Moon",
             "age": 10,
             "gpa": 4.0,
-            "category": this.ANIMAL,
+            "category": "animal",
         },
         {
             "id": 3,
             "name": "Star",
             "age": 7,
             "gpa": 4.0,
-            "category": this.ANIMAL,
+            "category": "animal",
         },
         {
             "id": 10,
             "name": "Joon",
             "age": 27,
             "gpa": 0,
-            "category": this.PERSON,
+            "category": "person"
         },
         {
             "id": 11,
             "name": "Toto",
             "age": 5,
             "gpa": 4.0,
-            "category": this.ANIMAL,
+            "category": "animal",
         },
         {
             "id": 12,
             "name": "Lee",
             "age": 25,
             "gpa": 3.8,
-            "category": this.PERSON,
+            "category": "person",
         },
         {
             "id": 13,
             "name": "Dangdangee",
             "age": 3,
             "gpa": 2.5,
-            "category": this.ANIMAL,
+            "category": "animal",
+        },
+        {
+            "id": 15,
+            "name": "ET",
+            "age": 9999999,
+            "gpa": 9999999,
+            "category": "unknow",
         },
     ]
 
     constructor(props) {
         super(props);
 
+        this.max_content_id = 15;
+
         this.state = {
-            bbs_category: this.ALL,
-            bbs_data: this.mock_student_data,
+            mode: "read",
+            student_list: this.mock_student_data,
+            selected_id: 0,
+            category: this.ALL,
+        }
+    }
+
+    renderDataList = () => {
+        const data_list = this.state.student_list;
+
+        if (data_list.length === 0) {
+            return (<li>No Data</li>);
+        }
+
+        if (this.state.category === "Home") {
+            return data_list.map(data => (
+                <li key={data.id}><a href={"./" + data.id} onClick={function(e) {
+                e.preventDefault();
+                this.setState({
+                    mode: "read",
+                    selected_id: data.id,
+                });
+            }.bind(this)}>{data.name}</a></li>));
+        }
+
+        return data_list.filter(data => data.category.toLocaleLowerCase() === this.state.category.toLocaleLowerCase())
+                        .map(data => (
+                            <li key={data.id}><a href={"./" + data.id} onClick={function(e) {
+                            e.preventDefault();
+                            this.setState({
+                                mode: "read",
+                                selected_id: data.id,
+                        });
+            }.bind(this)}>{data.name}</a></li>
+        ));
+    };
+
+    renderContent = () => {
+        if (this.state.mode === "read") {
+            return (<Student mode={this.state.mode} data={this.state.student_list.find(data => data.id === this.state.selected_id)}></Student>);
+        } else if (this.state.mode === "create") {
+            return (<StudentCreateForm current_category={this.state.category} onSubmit={function(name, age, gpa, category) {
+                this.max_content_id += 1;
+                this.setState({
+                    mode: "read",
+                    student_list: this.state.student_list.concat({
+                        id: this.max_content_id,
+                        name: name,
+                        age: age,
+                        gpa: gpa,
+                        category: category,
+                    }),
+                    selected_id: this.max_content_id,
+                });
+            }.bind(this)}></StudentCreateForm>);
         }
     }
 
     render() {
-        console.log("App Render");
+        console.log("BBS Render");
         return (
-            <div className="App">
-                <header><h1>Django-React Demo</h1></header>
+            <div className="BBS">
                 <nav>
                     <ul>
                         <li><a href="/" onClick={function(e) {
                             e.preventDefault();
                             this.setState({
-                                bbs_category: this.ALL,
-                                bbs_data: this.mock_student_data,
+                                mode: "read",
+                                selected_id: 0,
+                                category: this.ALL,
                             });
                         }.bind(this)}>{this.ALL}</a></li>
 
                         <li><a href="/person/" onClick={function(e) {
                             e.preventDefault();
                             this.setState({
-                                bbs_category: this.PERSON,
-                                bbs_data: this.mock_student_data.filter(data => data.category === this.PERSON),
+                                mode: "read",
+                                selected_id: 0,
+                                category: this.PERSON,
                             });
                         }.bind(this)}>{this.PERSON}</a></li>
 
                         <li><a href="/animal/" onClick={function(e) {
                             e.preventDefault();
                             this.setState({
-                                bbs_category: this.ANIMAL,
-                                bbs_data: this.mock_student_data.filter(data => data.category === this.ANIMAL),
+                                mode: "read",
+                                selected_id: 0,
+                                category: this.ANIMAL,
                             });
                         }.bind(this)}>{this.ANIMAL}</a></li>
                     </ul>
                 </nav>
-                <BBS category={this.state.bbs_category} data_list={this.state.bbs_data}></BBS>
+
+                <h2>{this.state.category}</h2>
+
+                <nav>
+                    <ul>
+                        {this.renderDataList()}
+                    </ul>
+                </nav>
+
+                <input type="button" value="Create" onClick={function(e){
+                    e.preventDefault();
+                    this.setState({
+                        mode: "create",
+                    });
+                }.bind(this)}></input>
+
+                {this.renderContent()}
+            </div>
+        );
+    }
+}
+
+class App extends Component {
+    render() {
+        console.log("App Render");
+        return (
+            <div className="App">
+                <header><h1>Django-React Demo</h1></header>
+                <BBS></BBS>
             </div>
         );
     }
