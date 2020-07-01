35d38d1a456c6b5e849d29abb1ce40146ac9f267
Jeongjoon Lee <joondev32@gmail.com>
2020-06-30 23:48:30 -0700

    refactor: Create template for studetn list view

--- app/src/components/BBS.js
+++ app/src/components/BBS.js
@@ -7,17 +7,22 @@ import All from './All';
 import Animal from './Animal';
 import Person from './Person';
 import StudentForm from './StudentForm';
+import StudentList from './StudentList';
 
 function BBS() {
+  function renderStudentList(category, props) {
+    return <StudentList {...props} category={category} />
+  }
+
   console.log("BBS Render");
   return (
     <div className="BBS">
       <CategoryTab />
 
       <Switch>
-        <Route exact path="/" component={All} />
-        <Route path="/person/" component={Person} />
-        <Route path="/animal/" component={Animal} />
+        <Route exact path="/" render={renderStudentList.bind(this, "")}/>
+        <Route path="/person/" render={renderStudentList.bind(this, "person")} />
+        <Route path="/animal/" render={renderStudentList.bind(this, "animal")} />
         <Route path="/student/create" component={StudentForm} />
         <Route path="/">
           Not Found
---
+++ app/src/components/StudentList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function StudentList({category}) {
+  return (
+    <div>
+      <h1>StudentList</h1>
+      <p>{category}</p>
+    </div>
+  );
+}
+
+export default StudentList;
